ea4da338b94c8071955d0f3ad9685fe18fc45acb
manyids2 <arun.mukundan@gmail.com>
2025-06-06 21:43:08 +0200

    YES: Test case for tree, but rudimentary for now

--- src/tree.c
+++ src/tree.c
@@ -1,11 +1,59 @@
 #include "tree.h"
+#include "arena.h"
+#include "array.h"
 #include "range.h"
 #include "slice.h"
 #include <assert.h>
+#include <stdio.h>
+
+void indexLevels(Tree* t, i32 i, i32 level)
+{
+    t->levels[i] = level;
+    RANGE(j, t->children[i].len) { indexLevels(t, t->children[i].data[j], level + 1); }
+}
+
+i32 indexOrder(Tree* t, i32 i, i32 count)
+{
+    count++;
+    t->order[count] = i;
+    RANGE(j, t->children[i].len) { count = indexOrder(t, t->children[i].data[j], count); }
+    return count;
+}
 
 Tree tree_from_children(i32 len, Ai32* children, Arena* perm)
 {
-    Tree t = {.len = len, .children = children};
+    Tree t = {
+        .len      = len,
+        .order    = new (perm, i32, len),
+        .levels   = new (perm, i32, len),
+        .parents  = new (perm, i32, len),
+        .children = new (perm, Ai32, len),
+    };
+    RANGE(i, len) { t.parents[i] = -1; }
+
+    // Store children, Index parents
+    RANGE(i, len)
+    {
+        t.children[i] = ai32_new(children[i].len, perm);
+        RANGE(j, children[i].len)
+        {
+            t.children[i].data[j] = children[i].data[j];
+
+            t.parents[t.children[i].data[j]] = i;
+        }
+    }
+
+    // Index order and levels
+    i32 count = -1;
+    RANGE(i, len)
+    {
+        if (t.parents[i] == -1)
+        {
+            indexLevels(&t, i, 0);
+            count = indexOrder(&t, i, count);
+        }
+    }
+
     return t;
 }
 
@@ -20,10 +68,13 @@ Tree tree_from_levels(i32 len, i32* levels, Arena* perm)
     };
 
     // First element is root
-    i32  level_root = levels[0];
-    Si32 stack      = si32_new(len, perm);
+    i32 level_root = levels[0];
+
+    Arena temp  = *perm;
+    Si32  stack = si32_new(len, &temp);
     si32_push(&stack, -1);
 
+    // Index parents (only one pass needed, no allocations)
     RANGE(i, len)
     {
         t.order[i]  = i;
@@ -46,6 +97,20 @@ Tree tree_from_levels(i32 len, i32* levels, Arena* perm)
         // Parent is at top of stack
         t.parents[i] = stack.data[stack.len - 1];
     }
+
+    // Index children (min 2 passes, 4 passes needed for proper inits, memory management)
+    Si32* children  = new (&temp, Si32, len);
+    i32*  nchildren = new (&temp, i32, len);
+    RANGE(i, len)
+    {
+        if (t.parents[i] >= 0) { nchildren[t.parents[i]]++; }
+    }
+    RANGE(i, len) { children[i] = si32_new(nchildren[i], perm); }
+    RANGE(i, len)
+    {
+        if (t.parents[i] >= 0) { si32_push(&children[t.parents[i]], i); }
+    }
+    RANGE(i, len) { t.children[i] = (Ai32){.len = children[i].len, .data = children[i].data}; }
 
     return t;
 }
--- test/test_tree_case1.c
+++ test/test_tree_case1.c
@@ -3,20 +3,27 @@
 #include "range.h"
 #include "str.h"
 #include "tree.h"
+#include <assert.h>
 #include <stdio.h>
 
 int main()
 {
-
     Arena perm = arena_new(CAPACITY);
 
     i32 len       = 8;
     i32 levels[8] = {0, 1, 1, 2, 1, 0, 1, 0};
 
-    i32 order[8]   = {0, 1, 2, 3, 4, 5, 6, 7};
-    i32 parents[8] = {-1, 0, 0, 2, 0, -1, 5, -1};
-    printf("i: order[i], parents[i]\n");
-    RANGE(i, 8) { printf("%d: %d, %2d\n", i, order[i], parents[i]); }
+    Tree tl = tree_from_levels(len, levels, &perm);
+    printf("Tree: %d\n", tl.len);
+    printf("i: order  level  parents  children\n");
+
+    Arena temp = perm;
+    RANGE(i, 8)
+    {
+        Str c = ai32_print(&tl.children[i], &temp);
+        //    ( "i: order  level  parents  children\n");
+        printf("%d: %2d     %2d     %2d       %.*s\n", i, tl.order[i], tl.levels[i], tl.parents[i], pstr(c));
+    }
 
     i32 c_0[3] = {1, 2, 4};
     i32 c_2[1] = {3};
@@ -27,14 +34,33 @@ int main()
     children[2]      = (Ai32){.len = 1, .data = c_2};
     children[5]      = (Ai32){.len = 1, .data = c_5};
 
-    Tree t     = tree_from_levels(len, levels, &perm);
-    t.children = children;
-    printf("Tree: %d\n", t.len);
-    printf("i: order[i], parents[i], children[i]\n");
+    Tree tc = tree_from_children(len, children, &perm);
+    printf("Tree: %d\n", tl.len);
+    printf("i: order  level  parents  children\n");
 
+    temp = perm;
     RANGE(i, 8)
     {
-        Str c = ai32_print(&t.children[i], &perm);
-        printf("%d: %d, %2d, %.*s\n", i, t.order[i], t.parents[i], pstr(c));
+        Str c = ai32_print(&tc.children[i], &temp);
+        //    ( "i: order  level  parents  children\n");
+        printf("%d: %2d     %2d     %2d       %.*s\n", i, tl.order[i], tl.levels[i], tl.parents[i], pstr(c));
+    }
+
+    // Expected
+    i32 order[8]   = {0, 1, 2, 3, 4, 5, 6, 7};
+    i32 parents[8] = {-1, 0, 0, 2, 0, -1, 5, -1};
+    RANGE(i, 8)
+    {
+        assert(tc.order[i] == order[i]);
+        assert(tc.levels[i] == levels[i]);
+        assert(tc.parents[i] == parents[i]);
+        assert(tc.children[i].len == children[i].len);
+        RANGE(j, tc.children[i].len) assert(tc.children[i].data[j] == children[i].data[j]);
+
+        assert(tl.order[i] == order[i]);
+        assert(tl.levels[i] == levels[i]);
+        assert(tl.parents[i] == parents[i]);
+        assert(tc.children[i].len == children[i].len);
+        RANGE(j, tc.children[i].len) assert(tc.children[i].data[j] == children[i].data[j]);
     }
 }
